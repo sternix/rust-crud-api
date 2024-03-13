b4474f8df6cedc33a184cba2d00ef8165f9956cb
Yavuz Tanrıverdi <sternix@gmail.com>
2024-03-13 16:25:37 +0300

    tabloları düzenledim

--- src/services/user.rs
+++ src/services/user.rs
@@ -25,7 +25,7 @@ async fn get_all(pool: web::Data<deadpool_postgres::Pool>) -> Result<HttpRespons
     log::info!("get_all get isteği geldi");
 
     let client = pool.get().await?;
-    let rows = client.query("SELECT * FROM user ORDER BY id ", &[]).await?;
+    let rows = client.query("SELECT * FROM users ORDER BY id ", &[]).await?;
     let users: Vec<User> = rows.iter().map(|r| User::from_row(r)).collect();
     Ok(HttpResponse::Ok().json(&users))
 }
@@ -41,7 +41,7 @@ async fn get_by_id(
     let client = pool.get().await?;
 
     let row = client
-        .query_opt("SELECT * FROM user WHERE id = $1 ", &[&id])
+        .query_opt("SELECT * FROM users WHERE id = $1 ", &[&id])
         .await?;
 
     if let Some(row) = row {
@@ -61,7 +61,7 @@ async fn insert(
     let client = pool.get().await?;
     client
         .execute(
-            " INSERT INTO user(name,email,phone) VALUES($1,$2,$3) ",
+            " INSERT INTO users(name,email,phone) VALUES($1,$2,$3) ",
             &[&user.name, &user.email, &user.phone],
         )
         .await?;
@@ -72,19 +72,23 @@ async fn insert(
 async fn update(
     pool: web::Data<deadpool_postgres::Pool>,
     body: web::Bytes,
+    path: web::Path<i32>,
 ) -> Result<HttpResponse, MyError> {
     log::info!("put isteği geldi");
+
+    let id = path.into_inner();
+
     let user = serde_json::from_slice::<User>(&body)?;
 
     let client = pool.get().await?;
     client
         .execute(
-            " UPDATE user SET name = $1, email = $2, phone = $3 WHERE id = $4 ",
-            &[&user.name, &user.email, &user.phone, &user.id],
+            " UPDATE users SET name = $1, email = $2, phone = $3 WHERE id = $4 ",
+            &[&user.name, &user.email, &user.phone, &id],
         )
         .await?;
 
-    Ok(HttpResponse::Ok().finish())
+    Ok(HttpResponse::Ok().json(&user))
 }
 
 async fn delete(
@@ -97,7 +101,7 @@ async fn delete(
 
     let client = pool.get().await?;
     client
-        .execute(" DELETE FROM user WHERE id = $1 ", &[&id])
+        .execute(" DELETE FROM users WHERE id = $1 ", &[&id])
         .await?;
 
     Ok(HttpResponse::Ok().finish())
@@ -108,12 +112,12 @@ pub fn init(cfg: &mut web::ServiceConfig) {
         web::resource("/user")
             .route(web::get().to(get_all))
             .route(web::post().to(insert))
-            .route(web::put().to(update)),
     );
 
     cfg.service(
         web::resource("/user/{id}")
             .route(web::delete().to(delete))
-            .route(web::get().to(get_by_id)),
+            .route(web::get().to(get_by_id))
+            .route(web::put().to(update))
     );
 }
